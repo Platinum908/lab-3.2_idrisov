53a0cfb74321382b184cadf3168a6cd116ab5f28
Administrator <Administrator@WIN-QH3366O2A51>
2023-11-17 23:50:21 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿//3.2 Высокий уровень #15
+
+Console.Write("Введите верхний предел: ");
+double n = double.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+double m = double.Parse(Console.ReadLine());
+double result = 0;
+for (int i = 1; i <= n; i++)
+{
+    double value = 1;
+    for (int j = 1; j <= m; j++)
+    {
+        value *= i;
+    }
+    result += value;
+}
+Console.WriteLine("Сумма чисел от 1 до {0}, возведенных в степень {1}, равна {2}", n, m, result);
